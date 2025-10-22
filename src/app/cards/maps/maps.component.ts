8f376f8f8e77cf3b671071b3bdd8eed1cbf24556
Preeti <preetiyadav5443@gmail.com>
2025-10-22 21:37:22 +0530

    Update contact email, adjust map URL, and modify social links in portfolio

--- src/app/cards/maps/maps.component.ts
+++ src/app/cards/maps/maps.component.ts
@@ -28,7 +28,7 @@ export class MapsComponent implements OnInit {
       this.mapUrl = this.sanitizer.bypassSecurityTrustResourceUrl(cachedUrl);
     } else {
       // The URL you want to cache and use
-      const url = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.6168395549!2d75.780411!3d11.258753!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba65c931f9a983d%3A0x4c34b7f3e6b71fa2!2sKozhikode%2C%20Kerala%2C%20India!5e0!3m2!1sen!2sus!4v1671220374408!5m2!1sen!2sus&zoom=12';
+      const url = 'https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3485.1752254350017!2d75.77839907497541!3d29.130021661316526!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjnCsDA3JzQ4LjEiTiA3NcKwNDYnNTEuNSJF!5e0!3m2!1sen!2sin!4v1761149135619!5m2!1sen!2sin';
 
       this.mapCacheService.setMapDetails(url);
       this.mapUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
